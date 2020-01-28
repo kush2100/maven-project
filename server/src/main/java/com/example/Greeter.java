1629d61bc4085fe7b701fd800a31dacfdcc93817
kaushik <kush210@gmail.com>
2020-01-28 16:04:46 +0530

    edit file

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,5 +1,4 @@
 package com.example;
-package-info.java
 
 /**
  * This is a class.
@@ -13,8 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  //TODOoo: Add javadoc comment
+  public  String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
